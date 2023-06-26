abef9c79e8017298654d0c17e70b0df488ce45a2
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-06-26 18:37:33 +0530

    notes

---
+++ Day-2/script.js
@@ -0,0 +1,11 @@
+debugger;
+let a = 100; //number
+//variable name - Identifier
+//value - Literals
+let b = 20;
+let c = a + b;
+
+let name = "Manikandan"; //string
+let passport = true; //boolean
+
+console.log(c);
