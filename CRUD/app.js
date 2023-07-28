2f9f3908542c6f3957dc90063e2260bb5c39c1b7
Manikandan Anbalagan <manikandana121290@gmail.com>
2023-07-28 20:55:17 +0530

    CRUD

--- CRUD/app.js
+++ CRUD/app.js
@@ -8,15 +8,14 @@ let data = [
 function displayData() {
   const dataRows = document.getElementById("dataRows");
   dataRows.innerHTML = "";
-  debugger;
   data.forEach((value, index) => {
     const row = `
         <tr>
             <td>${value.name}</td>
             <td>${value.age}</td>
             <td>
-                <button class="btn btn-info edit-btn">Edit</button>
-                <button class="btn btn-danger delete-btn">Delete</button>
+                <button class="btn btn-info edit-btn" data-index="${index}">Edit</button>
+                <button class="btn btn-danger delete-btn" data-index="${index}">Delete</button>
             </td>
         </tr>
     `;
@@ -44,9 +43,40 @@ function handleFormSubmit(event) {
   displayData();
 }
 
+// Function to handle delete button click
+function handleDeleteClick(event) {
+  const index = event.target.getAttribute("data-index");
+  data.splice(index, 1);
+  displayData();
+}
+
+// Function to handle edit button click
+function handleEditClick(event) {
+  const index = event.target.getAttribute("data-index");
+  const record = data[index];
+
+  const nameInput = document.getElementById("name");
+  const ageInput = document.getElementById("age");
+
+  nameInput.value = record.name;
+  ageInput.value = record.age;
+
+  data.splice(index, 1);
+  displayData();
+}
+
 // Attach event listeners
 document
   .getElementById("crudForm")
   .addEventListener("submit", handleFormSubmit);
+
+document.getElementById("dataRows").addEventListener("click", (s) => {
+  //alert(event.target.classList);
+  if (event.target.classList.contains("delete-btn")) {
+    handleDeleteClick(event);
+  } else if (event.target.classList.contains("edit-btn")) {
+    handleEditClick(event);
+  }
+});
 
 displayData();
