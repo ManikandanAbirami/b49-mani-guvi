26b0255ab3b86571ad64ca8c8516981daa4cbd9a
Manikandan Anbalagan <manikandana121290@gmail.com>
2023-08-22 19:04:38 +0530

    changes

--- React-FrontEnd/Day-6/my-app/src/Diet.jsx
+++ React-FrontEnd/Day-6/my-app/src/Diet.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function Diet() {
-    const { dietName } = useParams(); // Get the URL param "name"
+    const { dietName } = useParams(); // Get the URL param "dietName"
     
     const dietInfo = {
         lowCarb: "Low carb is best diet for weight loss!!",
