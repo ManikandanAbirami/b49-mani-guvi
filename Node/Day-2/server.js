35b4caa1f31e4675a6d38f63b7238d469b41c506
Manikandan Anbalagan <manikandana121290@gmail.com>
2023-09-14 21:04:13 +0530

    testfile

--- Node/Day-2/server.js
+++ Node/Day-2/server.js
@@ -2,29 +2,30 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const app = express();
+const outputFolder = "./output";
 
-const outputFolder = "./output"; //Folder name where files will be stored
-
-//To check whether folder is alreadt there, if not create a new folder
 if (!fs.existsSync(outputFolder)) {
   fs.mkdirSync(outputFolder);
 }
 
+const app = express();
 const PORT = 3000;
 
 app.get("/createFile", (req, res) => {
   const currentTime = new Date();
+
   const year = currentTime.getFullYear().toString();
   const month = (currentTime.getMonth() + 1).toString();
   const date = currentTime.getDate().toString();
-  const hrs = currentTime.getHours().toString();
+  const hours = currentTime.getHours().toString();
   const mins = currentTime.getMinutes().toString();
   const secs = currentTime.getSeconds().toString();
 
-  const dateTimeForFileName = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
+  const dateTimeForFileName = `${year}-${month}-${date}-${hours}-${mins}-${secs}.txt`;
 
   const filePath = path.join(outputFolder, dateTimeForFileName);
+
+  console.log("filePath:", filePath);
 
   fs.writeFile(filePath, currentTime.toISOString(), (err) => {
     if (err) {
@@ -39,10 +40,10 @@ app.get("/createFile", (req, res) => {
 app.get("/getFiles", (req, res) => {
   fs.readdir(outputFolder, (err, files) => {
     if (err) {
-      res.status(500).send(`Error reading files: ${err}`);
+      res.status(500).send(`Error reading directory: ${err}`);
       return;
     }
-    console.log("List of files:", files);
+    console.log("files", files);
     const textFiles = files.filter((file) => path.extname(file) === ".txt");
 
     res.json(textFiles);
